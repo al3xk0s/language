354a8a885cda3e279a54fafb317a1a69e50a1770
Косоурихин Алексей Александрович (яр) <a.kosourikhin@krista.ru>
2022-08-12 12:09:56 +0300

    Добавил контроль импортов по хэшам и названию файла

--- import.sh
+++ import.sh
@@ -1,12 +1,30 @@
+export IMPORT_SUMS=()
+
+function _containsImport() {
+    local targetHash="$1"
+    local currentHash
+    for currentHash in "${IMPORT_SUMS[@]}"; do
+        if [[ "$currentHash" == "$targetHash" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 function @import() {
     local path="$1"
-    local old_path="$(pwd)"
+    local oldPath="$(pwd)"
 
     local dir="$(dirname "$path")"
     local file="$(basename "$path")"
 
-    cd "$dir" && \
-    source "$file"
+    local fileHash
 
-    cd "$old_path"
+    cd "$dir" && \
+    fileHash="$(md5sum "$file")" && \
+    ! _containsImport "$fileHash" && \
+    source "$file" && \
+    IMPORT_SUMS[${#IMPORT_SUMS[@]}]="$fileHash"
+
+    cd "$oldPath"
 }
